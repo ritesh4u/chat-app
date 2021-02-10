94235f4731e6361b3eec24ae92805b97c481093b
Ritesh Niwane <riteshniwane007@gmail.com>
2021-02-10 18:45:38 +0530

    env changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,19 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { firebaseConfig } from "./env.local";
+
 export const environment = {
   production: false,
+  //below details kept in local file you can copy from your firebase console
   firebaseConfig: {
-    
+    apiKey: firebaseConfig.apiKey,
+    authDomain: firebaseConfig.authDomain,
+    projectId: firebaseConfig.projectId,
+    storageBucket: firebaseConfig.storageBucket,
+    messagingSenderId: firebaseConfig.messagingSenderId,
+    appId: firebaseConfig.appId,
+    measurementId: firebaseConfig.measurementId
   }
 };
 
