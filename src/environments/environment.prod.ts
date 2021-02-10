1eb1374cabd413614ebcb67a67eb33a757fb99ee
Ritesh Niwane <riteshniwane007@gmail.com>
2021-02-10 19:34:32 +0530

    prod env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+import { firebaseConfig } from "./env.local";
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: firebaseConfig.apiKey,
+    authDomain: firebaseConfig.authDomain,
+    projectId: firebaseConfig.projectId,
+    storageBucket: firebaseConfig.storageBucket,
+    messagingSenderId: firebaseConfig.messagingSenderId,
+    appId: firebaseConfig.appId,
+    measurementId: firebaseConfig.measurementId
+  }
 };
